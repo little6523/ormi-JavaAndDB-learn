ef92d8e96a8b676ce18cce8a2fac74469c030550
LeeHyeonjun <67815747+little6523@users.noreply.github.com>
2024-07-03 15:00:45 +0900

    Add files via upload

---
+++ src/spring-data-jpa-example/src/main/java/com/ormispring/springdatajpaexample/SpringDataJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ormispring.springdatajpaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaExampleApplication.class, args);
+    }
+
+}
---
+++ src/spring-data-jpa-example/src/main/java/com/ormispring/springdatajpaexample/domain/Students.java
@@ -0,0 +1,5 @@
+package com.ormispring.springdatajpaexample.domain;
+
+public class Students {
+
+}
